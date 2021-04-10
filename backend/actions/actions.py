df04f93c2dc4c7cc0de76b00c67cb063fc0a9e01
Krupesh Anadkat <kmanadkat@gmail.com>
2021-04-10 18:46:32 +0530

    Completed Rasa for Beginners Wellness bot

--- backend/actions/actions.py
+++ backend/actions/actions.py
@@ -1,46 +1,22 @@
-# from typing import Any, Text, Dict, List, Union
+from typing import Text, List
 
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-# from rasa_sdk.forms import FormAction
+from rasa_sdk import Tracker, FormValidationAction
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.types import DomainDict
 
-# class HealthForm(FormAction):
+class ValidateHealthForm(FormValidationAction):
 
-#   def name(self):
-#     return "health_form"
+  def name(self) -> Text:
+      return "validate_health_form"
 
-#   @staticmethod
-#   def required_slots(tracker: Tracker) -> List[Text]:
-#     if tracker.get_slot("confirm_exercise") == True:
-#       return ["confirm_exercise", "exercise", "sleep", "diet", "stress", "goal"]
-#     else:
-#       return ["confirm_exercise", "sleep", "diet", "stress", "goal"]
-
-#   async def submit(
-#     self,
-#     dispatcher: CollectingDispatcher,
-#     tracker: Tracker,
-#     domain: Dict[Text, Any]
-#   ) -> List[Dict]:
-#       return []
-
-#   def slot_mappings(self) -> Dict[Text, Union[Dict, List[Dict]]]:
-#       return {
-#         "confirm_exercise": [
-#           self.from_intent(intent="affirm", value=True),
-#           self.from_intent(intent="deny", value=False),
-#           self.from_intent(intent="inform", value=True)
-#         ],
-#         "sleep": [
-#           self.from_entity(entity="sleep"),
-#           self.from_intent(intent="deny", value="None"),
-#         ],
-#         "diet": [
-#           self.from_text(intent="inform"),
-#           self.from_text(intent="affirm"),
-#           self.from_text(intent="deny"),
-#         ],
-#         "goal": [
-#           self.from_text(intent="inform"),
-#         ]
-#       }
+  async def required_slots(
+    self,
+    slots_mapped_in_domain: List[Text],
+    dispatcher: "CollectingDispatcher",
+    tracker: "Tracker",
+    domain: "DomainDict"
+  ) -> List[Text]:
+    if tracker.get_slot("confirm_exercise") == True:
+      return ["confirm_exercise", "exercise", "sleep", "diet", "stress", "goal"]
+    else:
+      return ["confirm_exercise", "sleep", "diet", "stress", "goal"]
